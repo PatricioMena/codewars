34058a09cf8ff75713d76660fdd5201950be910a
Patricio Mena <luispatricio.mh@gmail.com>
2023-04-14 15:07:12 -0600

    4/14/23 codewars

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Exes and Ohs
+// Exes and Ohs (4/13/23)
 // Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 // Examples input/output:
 // XO("ooxx") => true
@@ -11,3 +11,8 @@ function XO(str) {
   let o = str.match(/o/gi);
   return (x && x.length) === (o && o.length);
 }
+
+// No odddities here (4/14/23)
+//Write a small function that returns the values of an array that are not odd.
+// All values in the array will be integers. Return the good values in the order they are given.
+const noOdds = (values) => values.filter((value) => value % 2 === 0);
