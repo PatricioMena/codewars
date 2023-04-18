3b7605c94d1856d5dbfe1b9f0244adb80c8de309
Patricio Mena <luispatricio.mh@gmail.com>
2023-04-17 20:02:06 -0600

    4/17/23 codewars

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ function XO(str) {
 //Write a small function that returns the values of an array that are not odd.
 // All values in the array will be integers. Return the good values in the order they are given.
 const noOdds = (values) => values.filter((value) => value % 2 === 0);
+
+// Sort array by string length
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+const arr = ['Telescopes', 'Glasses', 'Eyes', 'Monocles'];
+
+const sortByLength = (arr) => arr.sort((a, b) => a.length - b.length);
