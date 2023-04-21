bcb035782cb3bee62c0c3a91ff4ffd61f269f33a
Patricio Mena <luispatricio.mh@gmail.com>
2023-04-20 19:47:11 -0600

    4/20/23 Codewars

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const largest = (n, array) => array.sort((b, a) => a - b).slice(array.length - n
 
 console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
 
-//// Between Extremes
+//// <7 kyu> Between Extremes
 // Given an array of numbers, return the difference between the largest and smallest values.
 
 // For example:
@@ -52,3 +52,34 @@ console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
 // The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
 const betweenExtremes = (numbers) => console.log(...numbers);
 console.log(betweenExtremes([23, 3, 19, 21, 16]));
+
+//// <8 kyu> Counting sheep...
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// For example:
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+const countSheeps = (arrayOfSheep) => arrayOfSheep.filter(Boolean).length;
+
+// <7 kyu> Remove the minimum
+// Description:
+// The museum of incredible dull things
+
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+// Task
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+const removeSmallest = (numbers) => {
+  let min = Math.min(...numbers);
+  return numbers.splice();
+};
+
+console.log(removeSmallest([1, 2, 3, 4, 5]));
