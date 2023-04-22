9c87cc0c1f4c97dde03a0ec691e0c5d00277980c
Patricio Mena <luispatricio.mh@gmail.com>
2023-04-21 18:33:09 -0600

    4/21/23 codewars

--- main.js
+++ main.js
@@ -65,21 +65,9 @@ console.log(betweenExtremes([23, 3, 19, 21, 16]));
 // The correct answer would be 17.
 const countSheeps = (arrayOfSheep) => arrayOfSheep.filter(Boolean).length;
 
-// <7 kyu> Remove the minimum
-// Description:
-// The museum of incredible dull things
+//// <8 kyu> Calculate average
+// Write a function which calculates the average of the numbers in a given list.
+// Note: Empty arrays should return 0.
 
-// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
-
-// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
-// Task
-
-// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
-
-// Don't change the order of the elements that are left.
-const removeSmallest = (numbers) => {
-  let min = Math.min(...numbers);
-  return numbers.splice();
-};
-
-console.log(removeSmallest([1, 2, 3, 4, 5]));
+const findAverage = (array) =>
+  array.length === 0 ? 0 : array.reduce((acc, currVal) => acc + currVal, 0) / array.length;
